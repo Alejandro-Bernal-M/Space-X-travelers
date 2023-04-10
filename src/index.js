211f480ee36ddbfc87846c734c2c176a544a3d41
alejandro <alejandrober96@gmail.com>
2023-04-10 12:55:12 -0500

    Configuration of redux toolkit on index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <Provider store={store} >
+        <App />
+    </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
