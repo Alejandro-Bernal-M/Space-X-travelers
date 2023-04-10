bda31fde2d828d131cd4aa6726d420ecf7f8b3f0
alejandro <alejandrober96@gmail.com>
2023-04-10 12:51:28 -0500

    Add rockets slice file

---
+++ src/Rockets/RocketsSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState= [];
+
+const Rockets = createSlice({
+  name: 'Rockets',
+  initialState,
+  reducers: {},
+  extraReducers: {},
+})
+
+
+export  default Rockets.reducer ;
