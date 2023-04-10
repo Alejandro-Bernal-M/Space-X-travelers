25a2fba9c39390383d815d4c9f3790e3e166380e
David Espino <davidespinog@gmail.com>
2023-04-10 11:51:55 -0600

    Fix lint issues

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,30 @@
- import Logo from './planet.png';
- import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import Logo from './planet.png';
 
 const Navbar = () => {
   const links = [
-    {path: '/', text: 'Rockets'},
-    {path: 'missions', text: 'Missions'},
-    {path: 'myprofile', text: 'My profile'}
+    { path: '/', text: 'Rockets' },
+    { path: 'missions', text: 'Missions' },
+    { path: 'myprofile', text: 'My profile' },
   ];
 
   return (
     <nav className="navbar">
       <div className="nav-title">
-        <img className="logo" src={Logo} alt="logo"/>
-        <h1>Space Travelers' Hub</h1>
+        <img className="logo" src={Logo} alt="logo" />
+        <h1>Space Travelers&apos; Hub</h1>
       </div>
-      <ul className='nav-ul'>
-        {links.map((item) => {
-          return (
-          <li className='nav-li'>
+      <ul className="nav-ul">
+        {links.map((item) => (
+          <li key={item.text} className="nav-li">
             <NavLink className="navlink" to={item.path}>
               {item.text}
-            </NavLink>  
+            </NavLink>
           </li>
-          )
-        })}
+        ))}
       </ul>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
