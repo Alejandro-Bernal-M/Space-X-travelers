72ec64af44ef7f8145a61d9cb31b6831444a0b92
David Espino <davidespinog@gmail.com>
2023-04-10 11:18:51 -0600

    Fix lint errors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
 import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,7 +12,6 @@ root.render(
     <Provider store={store}>
       <App />
     </Provider>
-    
   </React.StrictMode>,
 );
 
