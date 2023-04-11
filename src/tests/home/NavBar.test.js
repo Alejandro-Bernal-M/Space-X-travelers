7dc7f61806084974462cf656c82ef19430859583
David Espino <davidespinog@gmail.com>
2023-04-11 17:05:21 -0600

    Fix linter issues

--- src/tests/home/NavBar.test.js
+++ src/tests/home/NavBar.test.js
@@ -1,12 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
 import render from '../utils/testUtils';
 import Navbar from '../../components/Navbar/Navbar';
-import { BrowserRouter } from 'react-router-dom';
+
 test('Home header test', () => {
   const { getByText, debug } = render(
     <BrowserRouter>
       <Navbar />
-    </BrowserRouter>
-  )
-  debug()
+    </BrowserRouter>,
+  );
+  debug();
   expect(getByText('Rockets')).toBeInTheDocument();
 });
