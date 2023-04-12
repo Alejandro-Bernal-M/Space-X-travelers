3809c4534512fac1e860019d6f78759c2b08bb24
alejandro <alejandrober96@gmail.com>
2023-04-12 15:50:46 -0500

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import { NavLink } from 'react-router-dom';
-import Logo from './planet.png';
 
 const Navbar = () => {
   const links = [
@@ -10,10 +9,7 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <div className="nav-title">
-        <img className="logo" src={Logo} alt="logo" />
-        <h1>Space Travelers&apos; Hub</h1>
-      </div>
+      <h1 className="nav-title">Space X Travelers</h1>
       <ul className="nav-ul">
         {links.map((item) => (
           <li key={item.text} className="nav-li">
